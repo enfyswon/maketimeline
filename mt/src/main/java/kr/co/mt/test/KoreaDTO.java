f5eec90942abc8e56c91b55c8147e78975d05855
jyjy0104 <110799069+jyjy0104@users.noreply.github.com>
2022-09-22 16:27:37 +0900

    insert

---
+++ mt/src/main/java/kr/co/mt/test/KoreaDTO.java
@@ -0,0 +1,34 @@
+package kr.co.mt.test;
+
+public class KoreaDTO {
+
+	private String tour_no;
+	private String tour_title;
+	private String tour_loc;
+	private String tour_desc;
+	public String getTour_no() {
+		return tour_no;
+	}
+	public void setTour_no(String tour_no) {
+		this.tour_no = tour_no;
+	}
+	public String getTour_title() {
+		return tour_title;
+	}
+	public void setTour_title(String tour_title) {
+		this.tour_title = tour_title;
+	}
+	public String getTour_loc() {
+		return tour_loc;
+	}
+	public void setTour_loc(String tour_loc) {
+		this.tour_loc = tour_loc;
+	}
+	public String getTour_desc() {
+		return tour_desc;
+	}
+	public void setTour_desc(String tour_desc) {
+		this.tour_desc = tour_desc;
+	}
+	
+}//class
